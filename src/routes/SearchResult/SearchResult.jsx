b84bde37c0fd2f7ee11393c93925566987a488b9
User <user@Users-MacBook-Pro.local>
2018-12-12 16:42:38 +0800

    update

--- src/routes/SearchResult/SearchResult.jsx
+++ src/routes/SearchResult/SearchResult.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import SearchInput from 'Components/SearchPage/SearchInput';
 import SearchQuestion from 'Components/SearchPage/SearchQuestion';
+import SearchCourse from 'Components/SearchPage/SearchCourse';
+
 import get from 'Utils/get';
 
 export default class SearchResult extends Component {
@@ -11,7 +13,7 @@ export default class SearchResult extends Component {
         return (
             <div style={{ background: '#fff', padding: '1rem' }}>
                 <SearchInput />
-
+                <SearchCourse />
                 <SearchQuestion word={word} />
             </div>
         );
